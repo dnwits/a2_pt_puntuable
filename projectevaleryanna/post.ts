6761f0e0cd8b9663f8d00ba86947d2dcb1abafd9
anpatricia-18 <ap.aguilera06@gmail.com>
2025-03-21 16:28:02 +0100

    Update post.ts
    canvis fets per Anna

--- projectevaleryanna/post.ts
+++ projectevaleryanna/post.ts
@@ -4,3 +4,4 @@ export type Post = {
     title: string
     body: string
 }
+//l'objecte Post és exportable, és a dir, es pot importar i reutilitzar en altres arxius Typescript on aquest objecte conté les propietats userId, id, title i body.
